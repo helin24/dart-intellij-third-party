fcdbf297d6b91c476b825b5bb78058cc6d3a1d95
Jaime Wren <jwren@google.com>
2025-11-10 15:05:16 -0800

    Update org.jetbrains.kotlin.jvm to 2.2.21 (#76)
    https://plugins.gradle.org/plugin/org.jetbrains.kotlin.jvm

--- third_party/build.gradle.kts
+++ third_party/build.gradle.kts
@@ -16,7 +16,7 @@ allprojects {
 // Plugins - must be first
 plugins {
     id("java") // Java support
-    id("org.jetbrains.kotlin.jvm") version "2.2.0" // Kotlin support
+    id("org.jetbrains.kotlin.jvm") version "2.2.21" // Kotlin support
     id("org.jetbrains.intellij.platform") version "2.7.2" // IntelliJ Platform Gradle Plugin
 }
 
